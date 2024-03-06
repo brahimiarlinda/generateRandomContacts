9276c304876cccb549a3c473bd2ddd2097e8a8e7
Arlinda Brahimi <46766925+brahimiarlinda@users.noreply.github.com>
2024-03-06 12:58:56 +0100

    Updated script based on new format

--- generateRandomContacts.js
+++ generateRandomContacts.js
@@ -3,14 +3,16 @@ const fs = require('fs');
 
 function generateRandomContact(numContacts, filePath) {
   const stream = fs.createWriteStream(filePath, {flags: 'w'});
-  stream.write("Email,First Name,Last Name,Phone Number\n");
+  stream.write("Phone number,Organisation name,First name,Last name,Email,Date of birth\n");
 
   for (let i = 0; i < numContacts; i++) {
+    const phoneNumber = faker.phone.number();
+    const organisationName = faker.person.firstName();
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
-    const email =faker.internet.email({ firstName, lastName })
-    const phoneNumber = faker.phone.imei();
-    stream.write(`${email},${firstName},${lastName},${phoneNumber}\n`);
+    const email = faker.internet.email(firstName,lastName);
+    const dateOfBirth = faker.date.past(30, new Date(2000, 0, 1)).toISOString().split('T')[0]; // Corrected method for date of birth
+    stream.write(`${phoneNumber},${organisationName},${firstName},${lastName},${email},${dateOfBirth}\n`);
   }
 
   stream.end();
